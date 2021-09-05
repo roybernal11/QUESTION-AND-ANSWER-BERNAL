057222183acd4dc198301aebba3eca8351676c9a
roybernal11 <32937378+roybernal11@users.noreply.github.com>
2021-09-05 13:53:26 +0800

    Quiz App

---
+++ script.js
@@ -0,0 +1,16 @@
+// start button function
+const startButton = document.getElementById('start-btn');
+
+startButton.addEventListener('click', startGame);
+
+function startGame(){
+    console.log('The Game Started!');
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+    
+}
